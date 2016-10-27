d19939963b24f9a535b47a1691627f9a90fcba78
Takumi MIURA <mitaku1104@gmail.com>
2016-10-27 12:03:12 +0900

    Schema導入

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from 'react-jsonschema';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
+    const schema = {
+      "title": "Basic Demo",
+      "type": "object",
+      "properties": {
+        "firstName": {
+          "type": "string",
+          "title": "First Name",
+        },
+        "lastName": {
+          "type": "string",
+          "title": "Last Name",
+        },
+        "age": {
+          "type": "integer",
+          "title": "Age",
+          "description": "Age in years",
+        },
+      },
+    };
+
+    const formData = {
+      firstName: '',
+      lastName: '',
+      age: '',
+    };
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+          </p>
+          <div>
+            <Form
+                schema={schema}
+                formData={formData}
+                onError={errors => {
+                  console.log(errors);
+                }}
+                onSubmit={data => {
+                  console.log(data);
+                }}
+              />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
